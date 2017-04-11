559afcff5cd1035e3512afe87d020b1ffb7730e9
Greg Rubin <grubin@catchpoint.com>
2017-04-11 15:50:33 -0400

    add animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  /*For the sticky nav*/
+  /*Add sticky class to nav*/
   $('.js--section-features').waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
@@ -32,5 +32,30 @@ $(document).ready(function() {
         }
       }
     });
+  });
+
+  /*Animations on scroll*/
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
   });
 });
