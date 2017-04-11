1f53dabb245c6d2804dc167fb53ee08b24c491fa
Greg Rubin <grubin@catchpoint.com>
2017-04-11 15:17:29 -0400

    added scroll to sections using html ID

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /*For the sticky nav*/
   $('.js--section-features').waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
@@ -9,12 +10,27 @@ $(document).ready(function() {
   offset: '60px;'
 });
 
-  /*
-  var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window')
-}, {
-  offset: '25%'
-})
-*/
+/*Scroll on buttons*/
+  $('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
 
+  /*Navingation scroll*/
+  $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 });
